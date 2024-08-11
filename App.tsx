ac20589a81f796e8fe901edf3d774a51b25cb5b7
Oscar Nogales <oer.nogales@yavirac.edu.ec>
2024-08-11 18:31:27 -0500

    correccion de tipeado y estructura

--- App.tsx
+++ App.tsx
@@ -10,95 +10,184 @@ import {
   Text,
   Dimensions,
 } from 'react-native';
-import { sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15 } from './sonidos/Sounds'; 
+import {
+  sound1,
+  sound2,
+  sound3,
+  sound4,
+  sound5,
+  sound6,
+  sound7,
+  sound8,
+  sound9,
+  sound10,
+  sound11,
+  sound12,
+  sound13,
+  sound14,
+  sound15,
+} from './sonidos/Sounds';
 import Sound from 'react-native-sound';
 
-const { width } = Dimensions.get('window');
-
+const {width} = Dimensions.get('window');
 // Función para reproducir el sonido
 const playSound = (sound: Sound) => {
-  console.log('Attempting to play sound...');
   sound.play((success: any) => {
     if (success) {
-      console.log('Sound played successfully');
-    } else {
-      console.log('Sound did not play');
+      return;
     }
   });
 };
-
 // Componente principal
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={styles.header.backgroundColor} />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={styles.header.backgroundColor}
+      />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View style={styles.header}>
           <Text style={styles.title}>Comunicador</Text>
         </View>
 
         <View style={styles.topPictogramsContainer}>
-          <TouchableOpacity onPress={() => playSound(sound1)} style={styles.pictogram}>
-            <Image source={require('./images/si.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound1)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/si.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Sí</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound2)} style={styles.pictogram}>
-            <Image source={require('./images/no.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound2)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/no.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>No</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.pictogramsContainer}>
-          <TouchableOpacity onPress={() => playSound(sound3)} style={styles.pictogram}>
-            <Image source={require('./images/comer.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound3)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/comer.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Comer</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound4)} style={styles.pictogram}>
-            <Image source={require('./images/papa.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound4)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/papa.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Papá</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound5)} style={styles.pictogram}>
-            <Image source={require('./images/mama1.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound5)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/mama1.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Mamá</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound6)} style={styles.pictogram}>
-            <Image source={require('./images/saludar.jpg')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound6)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/saludar.jpg')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Saludar</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound7)} style={styles.pictogram}>
-            <Image source={require('./images/feliz.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound7)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/feliz.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Feliz</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound8)} style={styles.pictogram}>
-            <Image source={require('./images/triste.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound8)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/triste.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Triste</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound9)} style={styles.pictogram}>
-            <Image source={require('./images/sed.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound9)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/sed.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Sed</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound10)} style={styles.pictogram}>
-            <Image source={require('./images/bano.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound10)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/bano.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Baño</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound11)} style={styles.pictogram}>
-            <Image source={require('./images/dolor.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound11)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/dolor.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Dolor</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound12)} style={styles.pictogram}>
-            <Image source={require('./images/jugar.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound12)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/jugar.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Jugar</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound13)} style={styles.pictogram}>
-            <Image source={require('./images/pintar.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound13)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/pintar.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Pintar</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound14)} style={styles.pictogram}>
-            <Image source={require('./images/musica.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound14)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/musica.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Musica</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => playSound(sound15)} style={styles.pictogram}>
-            <Image source={require('./images/abrazo.png')} style={styles.pictogramImage} />
+          <TouchableOpacity
+            onPress={() => playSound(sound15)}
+            style={styles.pictogram}>
+            <Image
+              source={require('./images/abrazo.png')}
+              style={styles.pictogramImage}
+            />
             <Text style={styles.pictogramText}>Abrazo</Text>
           </TouchableOpacity>
         </View>
@@ -106,8 +195,6 @@ const App = () => {
     </SafeAreaView>
   );
 };
-
-// Estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -126,7 +213,7 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontFamily: 'Cursive',
     textShadowColor: 'orange',
-    textShadowOffset: { width: -1, height: 1 },
+    textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 8,
   },
   topPictogramsContainer: {
@@ -150,9 +237,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
-    // Agregar sombra para efecto 3D
     shadowColor: '#000',
-    shadowOffset: { width: 5, height: 5 },
+    shadowOffset: {width: 5, height: 5},
     shadowOpacity: 0.9,
     shadowRadius: 5,
     elevation: 11, // Para Android
@@ -170,5 +256,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
-
 export default App;
--- sonidos/Sounds.tsx
+++ sonidos/Sounds.tsx
@@ -1,124 +1,92 @@
-// Sounds.tsx (o Sounds.js)
+import {Alert} from 'react-native';
 import Sound from 'react-native-sound';
 
-// Cargar los sonidos
 export const sound1 = new Sound('si.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: si.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound2 = new Sound('no.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: no.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound3 = new Sound('comer.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: comer.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
-
 export const sound4 = new Sound('papa.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: papa.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound5 = new Sound('mama.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: mama.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound6 = new Sound('saludo.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: saludo.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound7 = new Sound('feliz.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: feliz.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound8 = new Sound('triste.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: triste.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound9 = new Sound('sed.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: sed.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound10 = new Sound('bano.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: bano.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound11 = new Sound('dolor.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: dolor.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound12 = new Sound('jugar.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: jugar.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound13 = new Sound('pintar.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: pintar.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound14 = new Sound('musica.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: musica.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
 
 export const sound15 = new Sound('abrazo.mp3', Sound.MAIN_BUNDLE, error => {
   if (error) {
-    console.log('Falla al cargar el sonido', error);
-  } else {
-    console.log('El sonido se cargo correctamente: abrazo.mp3');
+    Alert.alert('Error', 'Falla al cargar el sonido');
   }
 });
